3296d8b484946433d2d4621c19e63921c658a89f
Pietro Dessotti <pietrohdessotti@gmail.com>
2024-04-23 09:25:17 -0300

    feat(modules): created views to render a view Home or others

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import { Header } from '@components/Header'
+
+export const Home = (): JSX.Element => {
+   return (
+      <View>
+         <Header />
+      </View>
+   )
+}
