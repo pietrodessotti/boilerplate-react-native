36963b4c09f23315bd04766126b24ccbbb3eb277
Pietro Dessotti <pietrohdessotti@gmail.com>
2024-04-23 09:35:27 -0300

    style: configure babel, tsconfig, prettier and eslin.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigations': './src/navigation',
+          '@services': './src/services',
+          '@types': './src/types',
+          '@views': './src/views',
+        },
+      },
+    ],
+  ],
 };
